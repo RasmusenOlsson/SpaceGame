5d07d6060afd67dc53e18bd908abc1dbd059116f
RasmusenOlsson <rasmus.olsson@elev.ga.lbs.se>
2026-02-04 18:48:50 +0100

    Removed sounds cuz github

--- Assets/Scenes/ghj/CodesMm/Player.cs
+++ Assets/Scenes/ghj/CodesMm/Player.cs
@@ -6,11 +6,6 @@ public class GravityPlayer : MonoBehaviour
     [Header("View Reference")]
     public Transform viewTransform;
 
-    [Header("SoundEffects")]
-    [SerializeField] private AudioClip footStepSound;
-    [SerializeField] private AudioClip landSound;
-
-
     [Header("Movement")]
     public float moveSpeed = 6f;
     public float airSpeedMult = 0.4f;
@@ -101,7 +96,6 @@ public class GravityPlayer : MonoBehaviour
 
     void MovePlayer()
     {
-        SoundManager.instance.PlaySoundFXclip(footStepSound, transform, 1f);
         Vector3 gDir = gDirection;
         Vector3 viewForward = Vector3.ProjectOnPlane(viewTransform.forward, gDir).normalized;
         Vector3 viewRight = Vector3.Cross(-gDir, viewForward).normalized;
